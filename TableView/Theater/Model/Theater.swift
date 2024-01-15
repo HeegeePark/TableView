f23d4e292da7a4758c671ff9da6818bb0b6c4674
kizi <phg980513@naver.com>
2024-01-15 22:24:32 +0900

    [#10] 추가: Theater 구조체 구현

---
+++ TableView/Theater/Model/Theater.swift
@@ -0,0 +1,18 @@
+//
+//  Theater.swift
+//  TableView
+//
+//  Created by 박희지 on 1/15/24.
+//
+
+import Foundation
+
+// Theater(type: "롯데시네마", location: "롯데시네마 서울대입구", latitude: 37.4824761978647, longitude: 126.9521680487202),
+
+struct Theater {
+    let type: String
+    let location: String
+    let latitude: Double
+    let longitude: Double
+}
+
